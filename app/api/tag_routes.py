3cbce9f6a42f5f94e47d94042f605f576a483fdb
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-11-25 16:15:01 -0700

    login required and still working on the delete tag route4

--- app/api/tag_routes.py
+++ app/api/tag_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, session, request
+from flask_login import login_required
 from app.models import Tag, Note, db
 
 tag_routes = Blueprint('tags', __name__)
@@ -8,6 +9,7 @@ tag_routes = Blueprint('tags', __name__)
 
 
 @tag_routes.route('/', methods=['POST'])
+@login_required
 def createTag(userid):
     tag_name = str(request.data)
     tag_name = tag_name[2:-1]
@@ -22,5 +24,6 @@ def createTag(userid):
 
 
 @tag_routes.route('/<int:tagid>', methods=['DELETE'])
-def deleteTag(tagid):
+# @login_required
+def deleteTag(userid, tagid):
     Tag.delete().where(Tag.id == tagid)

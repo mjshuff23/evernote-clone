2a93d962c7f58813dd9d0c3c88870338ae4218c5
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-11-25 18:21:31 -0500

    post and put notebook routes functional

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,7 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
-app.register_blueprint(notebook_routes, url_prefix='/api/users/<int:user_id>/notebooks')
+app.register_blueprint(notebook_routes, url_prefix='/api/users/<int:userid>/notebooks')
 db.init_app(app)
 Migrate(app, db)
 
--- app/api/notebook_routes.py
+++ app/api/notebook_routes.py
@@ -1,25 +1,50 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, session
 from flask_login import login_required
-from app.models import Notebook
+from app.models import Notebook, db
+from sqlalchemy.sql import func
+
 
 notebook_routes = Blueprint('notebook', __name__)
 
+
 # create a new notebook
 @notebook_routes.route('/', methods=['POST'])
-@login_required
-def create_notebook(user_id):
-    pass
+# @login_required
+def create_notebook(userid):
+    title = str(request.data)[2:-1]
+    user_id = userid
+    new_notebook = Notebook(title=title, user_id=user_id)
+    db.session.add(new_notebook)
+    db.session.commit()
+    return {
+        'id': new_notebook.id,
+        'title': new_notebook.title,
+        'user_id': new_notebook.user_id,
+        'created_at': new_notebook.created_at,
+        'updated_at': new_notebook.updated_at
+    }
 
 
 # edit an existing notebook
-@notebook_routes.route('/<int:notebook_id>', methods=['PUT'])
-@login_required
-def edit_notebook(user_id, notebook_id):
-    pass
+@notebook_routes.route('/<int:notebookid>', methods=['PUT'])
+# @login_required
+def edit_notebook(userid, notebookid):
+    title = str(request.data)[2:-1]
+    notebook = Notebook.query.filter(Notebook.id == notebookid).first()
+    notebook.title = title
+    notebook.updated_at = func.now()
+    db.session.commit()
+    return {
+        'id': notebook.id,
+        'title': notebook.title,
+        'user_id': notebook.user_id,
+        'created_at': notebook.created_at,
+        'updated_at': notebook.updated_at
+    }
 
 
-# delete an existing notebook (first :id is user's id, second :id is notebook_id)
-@notebook_routes.route('/<int:notebook_id>', methods=['DELETE'])
-@login_required
+# delete an existing notebook
+@notebook_routes.route('/<int:notebookid>', methods=['DELETE'])
+# @login_required
 def delete_notebook(user_id, notebook_id):
     pass

adba14c9bedda6131d6c9e9ba04c0abda730b352
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-11-25 17:32:09 -0700

    added some seed data

--- app/seeds/notes.py
+++ app/seeds/notes.py
@@ -5,8 +5,38 @@ from app.models import db, Note
 
 def seed_notes():
 
-    demo_note = Note(title='Test', content='jehbwhefwehbdajlksdbkjsdbjlsdbljhsbdjhf', user_id=1, notebook_id=1)
-
+    demo_note = Note(
+        title='Test', content='jehbwhefwehbdajlksdbkjsdbjlsdbljhsbdjhf', user_id=1, notebook_id=1)
+    db.session.add(demo_note)
+    demo_note = Note(title='Another Note',
+                     content='bkjsdbjlsdbljhsbdjhf', user_id=1, notebook_id=1)
+    db.session.add(demo_note)
+    demo_note = Note(title='Programming Notes',
+                     content='<code/>', user_id=1, notebook_id=1)
+    db.session.add(demo_note)
+    demo_note = Note(title='My Super Cool Note',
+                     content='something super cool is here for sure', user_id=1, notebook_id=1)
+    db.session.add(demo_note)
+    demo_note = Note(title='This one is personal',
+                     content='My personal journal... don\'t read this... or else', user_id=1, notebook_id=1)
+    db.session.add(demo_note)
+    demo_note = Note(
+        title='La La La', content='do re mi fa so la ti do', user_id=1, notebook_id=1)
+    db.session.add(demo_note)
+    demo_note = Note(title='Wonderful :D',
+                     content='Isn\'t this note just wonderful? <3 ^_^', user_id=1, notebook_id=1)
+    db.session.add(demo_note)
+    demo_note = Note(title='More notes', content='a b c d e f g :)',
+                     user_id=1, notebook_id=1)
+    db.session.add(demo_note)
+    demo_note = Note(
+        title='Puppies', content='Mason, Misa, Buddy, Beth, and Macho', user_id=1, notebook_id=1)
+    db.session.add(demo_note)
+    demo_note = Note(
+        title='Fun stuff', content='super fun super fun, let\'s do stuff', user_id=1, notebook_id=1)
+    db.session.add(demo_note)
+    demo_note = Note(title='Grocery list',
+                     content='milk, eggs, soda, candy, bread, veggies, soap, sponges', user_id=1, notebook_id=1)
     db.session.add(demo_note)
     db.session.commit()
 

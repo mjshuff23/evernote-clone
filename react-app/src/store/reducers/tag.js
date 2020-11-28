e018cede032e50bfd40fb936557216072260c537
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-11-27 18:56:01 -0700

    working on the tag slice of state

--- react-app/src/store/reducers/tag.js
+++ react-app/src/store/reducers/tag.js
@@ -8,12 +8,17 @@ const initialState = {
 export default function reducer(state = initialState, action) {
     switch (action.type) {
         case CREATE_TAG:
-
+            let newState = { ...state };
+            newState[ action.tag.id ] = action.tag;
+            newState.ids.push(action.tag.id);
+            return newState;
         case DELETE_TAG:
-
+            let newState = { ...state };
+            delete newState[ action.tagid ];
+            newState = newState.ids.filter(id => id != action.tagid);
+            return newState;
         case TAG_NOTE:
         case UNTAG_NOTE:
-            return state; // change this once the add tag to note route is done
         default:
             return state;
     }

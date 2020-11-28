571d1a7998828098befe0039906d90e392c77df8
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-11-27 22:21:23 -0500

    finished notebook actions and reducers for now

--- react-app/src/store/reducers/notebooks.js
+++ react-app/src/store/reducers/notebooks.js
@@ -1,4 +1,4 @@
-import { CREATE_NOTEBOOK, RENAME_NOTEBOOK, DELETE_NOTEBOOK } from '../actions/notebook';
+import { SET_NOTEBOOKS, CREATE_NOTEBOOK, RENAME_NOTEBOOK, DELETE_NOTEBOOK } from '../actions/notebook';
 
 const initialState = {
     dict: {},
@@ -7,15 +7,19 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
+        case SET_NOTEBOOKS:
+            return action.notebooks;
+        default:
+        return state;
         case RENAME_NOTEBOOK:
         case CREATE_NOTEBOOK:
             const newState = { ...state }
-            newState[action.notebook.id] = action.notebook
+            newState.dict[action.notebook.id] = action.notebook
             newState.ids.push(action.notebook.id);
             return newState;
         case DELETE_NOTEBOOK:
             const newState = { ...state };
-            delete newState[action.notebookid];
+            delete newState.dict[action.notebookid];
             newState = newState.ids.filter(id => id != action.notebookid);
             return newState;
         default:

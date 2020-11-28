6b96193b4927eafd53c1ebb7728a6a81d66de282
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-11-27 21:29:35 -0500

    notebook actions partially done

--- react-app/src/store/actions/notebooks.js
+++ react-app/src/store/actions/notebooks.js
@@ -17,13 +17,34 @@ export const deleteNotebook = (notebookid) => ({
 
 
 export const createNotebookThunk = (title) => async (dispatch) => {
-
+    const newNotebook = await fetch(`/api/users/${userid}/notebooks/`, {
+        method: 'POST',
+        body: title
+    });
+    if (newNotebook.ok) {
+        newNotebook = await newNotebook.json();
+        dispatch(createNotebook(newNotebook));
+    }
 };
 
 export const renameNotebookThunk = (title) => async (dispatch) => {
-
+    const renamed = await fetch(`api/users/${userid}/notebooks/${notebookid}`, {
+        method: 'PUT',
+        body: title
+    });
+    if (renamed.ok) {
+        renamed = await renamed.json();
+        dispatch(renameNotebook(renamed.title))
+    }
 };
 
-export const deleteNotebookThunk = () => async (dispatch) => {
 
+export const deleteNotebookThunk = () => async (dispatch) => {
+    const deleted = await fetch(`api/users/${userid}/notebooks/${notebookid}`, {
+        method: 'DELETE'
+    });
+    if (deleted.ok) {
+        deleted = await deleted.json();
+        dispatch(deleteNotebook(deleted.id))
+    }
 };

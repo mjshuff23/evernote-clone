608a51f95d31ccf29983e2c868830585a7d3a98c
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-11-29 13:18:57 -0700

    fixed configureStore conflict

--- react-app/src/store/configureStore.js
+++ react-app/src/store/configureStore.js
@@ -1,25 +1,17 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from "redux-thunk";
 import user from './reducers/user';
-<<<<<<< HEAD
 import notebooks from './reducers/notebooks';
 import notes from './reducers/notes';
 import tags from './reducers/tags';
-=======
-import tag from './reducers/tag';
->>>>>>> created the basic shell for the reducer and actions for tags and note tags, and I created the routes for note tags
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducer = combineReducers({
     user,
-<<<<<<< HEAD
     notebooks,
     notes,
     tags
-=======
-    tag
->>>>>>> created the basic shell for the reducer and actions for tags and note tags, and I created the routes for note tags
 });
 
 const configureStore = (initialState) => {

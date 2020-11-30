0a5f94ee833b7f1d9104e8fc3c24b2e6ceaf1008
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-11-30 18:55:48 -0500

    import instead of form

--- react-app/src/components/NbPanelDialogue.js
+++ react-app/src/components/NbPanelDialogue.js
@@ -9,7 +9,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import CreateNewFolderIcon from '@material-ui/icons/CreateNewFolder';
-import { CreateNewNotebookThunk }
+// import { CreateNewNotebookThunk }
 
 const useStyles = makeStyles((theme) => ({
 

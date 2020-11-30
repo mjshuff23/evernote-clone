d6e4e4f532acba89bf80c60b92c3018ef31fa495
Michael Shuff <mjshuff23@gmail.com>
2020-11-30 12:29:15 -0500

    current note showing in editor

--- react-app/src/components/Editor.js
+++ react-app/src/components/Editor.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import ReactQuill from 'react-quill';
 import useStyles from './styles/EditorStyles';
 import debounce from 'lodash/debounce';
-
 
 
 export default function Editor() {
@@ -10,9 +10,18 @@ export default function Editor() {
     const [text, setText] = useState('');
     const [title, setTitle] = useState('');
     const [id, setId] = useState('');
+    const uiState = useSelector(state => state.ui);
+    const notesState = useSelector(state => state.notes);
+
     // const update = debounce(() => {
     //     console.log('UPDATING DB');
     // }, 2000);
+
+    useEffect(() => {
+        if (uiState.current_note) {
+            updateBody(notesState.dict[uiState.current_note].content);
+        }
+    }, [uiState.current_note]);
 
     function updateBody(text) {
         setText(text);

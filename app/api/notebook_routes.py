da28399e10785abbfa3c7c3e884edd3de4dcf85b
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-11-30 18:56:21 -0500

    import instead of form

--- app/api/notebook_routes.py
+++ app/api/notebook_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request, session
 from flask_login import login_required
 from app.models import Notebook, db
 from sqlalchemy.sql import func
-from json
+import json
 
 
 notebook_routes = Blueprint('notebook', __name__)

0b724f3035f96ad28d73258ae417ef76424f47a0
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-11-29 19:57:28 -0700

    working on the header of the tags panel

--- react-app/src/components/styles/TagPanelStyles.js
+++ react-app/src/components/styles/TagPanelStyles.js
@@ -2,7 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   tagPanel: {
-    left: 250,
+    backgroundColor: theme.palette.background.paper,
+    left: 200,
     zIndex: 1,
     position: 'absolute',
     display: 'flex',
@@ -25,6 +26,9 @@ const useStyles = makeStyles(theme => ({
   ul: {
     backgroundColor: "inherit",
     padding: 0
+  },
+  tagPanelHeader: {
+    padding: 5
   }
 }));
 

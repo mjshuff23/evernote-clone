cde398b2655e61bce185a1c2de809669ca82665f
Bonnie Hardie <bonniemariehardie@gmail.com>
2020-11-30 18:15:38 -0500

    added reducers and actions to this branch

--- react-app/src/store/actions/notebooks.js
+++ react-app/src/store/actions/notebooks.js
@@ -1,3 +1,56 @@
 export const SET_NOTEBOOKS = 'notebooks/SET_NOTEBOOKS';
+export const CREATE_NOTEBOOK = 'notebooks/CREATE_NOTEBOOKS';
+export const RENAME_NOTEBOOK = 'notebooks/RENAME_NOTEBOOKS';
+export const DELETE_NOTEBOOK = 'notebooks/DELETE_NOTEBOOKS';
 
-export const setNotebooks = (notebooks) => ({ type: SET_NOTEBOOKS, notebooks });
+
+export const setNotebooks = (notebooks) => ({
+    type: SET_NOTEBOOKS,
+    notebooks
+});
+export const createNotebook = (notebook) => ({
+    type: CREATE_NOTEBOOK,
+    notebook
+});
+export const renameNotebook = (notebook) => ({
+    type: RENAME_NOTEBOOK,
+    notebook
+});
+export const deleteNotebook = (notebookid) => ({
+    type: DELETE_NOTEBOOK,
+    notebookid
+});
+
+
+export const createNotebookThunk = (userid, title) => async (dispatch) => {
+    const newNotebook = await fetch(`/api/users/${userid}/notebooks/`, {
+        method: 'POST',
+        body: JSON.stringify(title)
+    });
+    if (newNotebook.ok) {
+        newNotebook = await newNotebook.json();
+        dispatch(createNotebook(newNotebook));
+    }
+};
+
+export const renameNotebookThunk = (userid, notebookid, title) => async (dispatch) => {
+    const renamed = await fetch(`api/users/${userid}/notebooks/${notebookid}`, {
+        method: 'PUT',
+        body: JSON.stringify(title)
+    });
+    if (renamed.ok) {
+        renamed = await renamed.json();
+        dispatch(renameNotebook(renamed.title))
+    }
+};
+
+
+export const deleteNotebookThunk = (userid, notebookid) => async (dispatch) => {
+    const deleted = await fetch(`api/users/${userid}/notebooks/${notebookid}`, {
+        method: 'DELETE'
+    });
+    if (deleted.ok) {
+        deleted = await deleted.json();
+        dispatch(deleteNotebook(deleted.id))
+    }
+};

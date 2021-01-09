bdccdc44c43455e7e864c82da89656b31f5433e0
Harrison Higgins <harryrhiggins@gmail.com>
2021-01-08 19:39:58 -0700

    I think I fixed the favicon on Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -77,6 +77,9 @@ def inject_csrf_token(response):
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+    if path.startswith('favicon')
+      or path.startswith('android')
+      or path.startswith('apple')
+      or path.startswith('mstile'):
+        return app.send_static_file(path)
     return app.send_static_file('index.html')

c434639e022b45c757b04da2c463de94c0e00e2d
Harrison Higgins <harryrhiggins@gmail.com>
2021-01-11 14:13:17 -0700

    Finished the notecard styling.

--- react-app/src/components/Notecard.js
+++ react-app/src/components/Notecard.js
@@ -1,13 +1,37 @@
 import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Typography from "@material-ui/core/Typography";
 import useStyles from './styles/NotecardStyles';
 import { Chip, ListItem } from "@material-ui/core";
-import { useParams, useHistory, NavLink } from 'react-router-dom';
-import { removeHTMLTags } from '../services/utils';
-import { removeTagFromNoteThunk } from '../store/actions/notes';
 import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import { useRouteMatch, NavLink } from 'react-router-dom';
+import { removeHTMLTags } from '../services/utils';
 import Moment from 'react-moment';
+
+function tagList(tag_ids, tags, classes) {
+  if (tag_ids.length === 0) return null;
+
+  const extra = tag_ids.slice(2).length;
+
+  tag_ids = tag_ids.slice(0, 2);
+
+  const jsx = (tag_ids.map(id => (
+    <Chip
+      size='small'
+      icon={<LocalOfferIcon />}
+      className={classes.singleTag}
+      label={tags.dict[id].title.length < 7 ?
+        tags.dict[id].title :
+        tags.dict[id].title.slice(0, 4) + '...'} />
+  )));
+
+  if (extra) {
+    jsx.push(
+      <Chip size='small' className={classes.singleTag} label={'+' + extra} />
+    )
+  }
+  return jsx;
+}
 
 function displayContent(content) {
   content = removeHTMLTags(content);
@@ -18,61 +42,42 @@ function displayContent(content) {
   }
 }
 
+
 export default function NoteCard({ noteId }) {
-  const dispatch = useDispatch();
-  let history = useHistory();
-  const notes = useSelector(state => state.notes);
+  const note = useSelector(state => state.notes.dict[noteId]);
   const tags = useSelector(state => state.tags);
-  const user = useSelector(state => state.user);
-
   const classes = useStyles();
-  let { current_notebook, current_note, current_tag } = useParams();
+  const match = useRouteMatch();
 
-  const removeTag = (tagId) => {
-    history.push(`/notebooks/${current_notebook}/notes/${current_note}/tags/${current_tag === tagId ? 'none' : current_tag}`);
-    dispatch(removeTagFromNoteThunk(noteId, tagId));
-    window.location.reload(false);
-  }
+  if (!noteId) return null;
 
-  if (!noteId || !Object.keys(notes.dict).length || !Object.keys(tags.dict).length || !notes.dict[noteId]) return null;
   return (
     <ListItem
       button
       className={classes.listitem}
       activeClassName={classes.selected}
       component={NavLink}
-      to={`/notebooks/${current_notebook}/notes/${noteId}/tags/${current_tag}`}
+      to={`/notebooks/${match.params.current_notebook}/notes/${noteId}/tags/${match.params.current_tag}`}
     >
       <div className={classes.notecard}>
         <div>
           <Typography className={classes.title} variant='h6'>
-            {notes.dict[noteId].title}
+            {note.title}
           </Typography>
         </div>
         <div className={classes.content}>
           <Typography variant="body1">
-            {displayContent(notes.dict[noteId].content)}
+            {displayContent(note.content)}
           </Typography>
         </div>
         <div className={classes.infobar}>
           <Typography variant='body2' component='div'>
-            <Moment format='LL' >
-              {notes.dict[noteId].updated_at}
+            <Moment format='ll' className={classes.date}>
+              {note.updated_at}
             </Moment>
           </Typography>
-          <div>
-            {notes.dict[noteId].tag_ids.map(tagId => (
-              <Chip
-                size='small'
-                icon={<LocalOfferIcon />}
-                className={classes.singleTag}
-                key={tagId}
-                label={tags.dict[tagId].title.length < 12 ?
-                  tags.dict[tagId].title :
-                  tags.dict[tagId].title.slice(0, 10) + '...'}
-                onClick={e => e.preventDefault()}
-                onDelete={e => { e.preventDefault(); removeTag(tagId) }} />
-            ))}
+          <div className={classes.tags}>
+            {tagList(note.tag_ids, tags, classes)}
           </div>
         </div>
       </div>
--- react-app/src/components/styles/NotecardStyles.js
+++ react-app/src/components/styles/NotecardStyles.js
@@ -2,50 +2,51 @@ import { makeStyles } from '@material-ui/core/styles';
 import { emphasize } from '@material-ui/core/styles/colorManipulator';
 
 const useStyles = makeStyles(theme => ({
-    notecard: {
-        width: '100%',
-        borderBottom: "1px solid black",
-        display: 'flex',
-        flexDirection: 'column',
-        padding: theme.spacing(2),
-    },
-    title: {
-        // fontSize: '1.2rem',
-        fontWeight: 'bold',
-    },
-    content: {
-        paddingTop: theme.spacing(1),
-        paddingBottom: theme.spacing(1),
-        wordWrap: 'break-word',
-    },
-    infobar: {
-        display: 'flex',
-        flexDirection: 'column',
-        width: '100%',
-        // justifyContent: 'space-between',
-    },
-    listitem: {
-        padding: 0,
-    },
-    selected: {
-        border: "2px solid #008F26",
-    },
-    cardTags: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    singleTag: {
-        margin: '0.25em 0.25em 0.25em 0',
-        WebkitTapHighlightColor: theme.palette.common.transparent,
-        cursor: 'pointer',
-        '&:focus': {
-            backgroundColor: emphasize('#008000', 0.08),
-        },
-        '&:active': {
-            boxShadow: theme.shadows[1],
-            backgroundColor: emphasize('#008000', 0.12),
-        },
+  notecard: {
+    width: '100%',
+    borderBottom: "1px solid black",
+    display: 'flex',
+    flexDirection: 'column',
+    padding: theme.spacing(2),
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+  content: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  infobar: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  tags: {
+    paddingLeft: theme.spacing(1),
+    display: 'flex',
+    flexWrap: 'nowrap',
+    alignItems: 'center',
+    width: '100%',
+  },
+  listitem: {
+    padding: 0,
+  },
+  selected: {
+    border: "2px solid #008F26",
+  },
+  date: {
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  singleTag: {
+    fontSize: '0.6rem',
+    margin: '0.25em 0.25em 0.25em 0',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '0.5rem',
     }
+  }
 }));
 
 export default useStyles;
